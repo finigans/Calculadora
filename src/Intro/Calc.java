4833af07908ff4f7f147b26bd9d834f1a0573d11
Jonathan Almeida <finigans@outlook.com.br>
2024-10-09 17:00:55 -0300

    Alinhando o display da calculadora

--- src/Intro/Calc.java
+++ src/Intro/Calc.java
@@ -2,10 +2,11 @@ package Intro;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
@@ -15,10 +16,34 @@ public class Calc extends JFrame{
 		super("calculadora");
 		
 		Container n = new JPanel();
-		n.setLayout(new GridLayout(4,4));
+		n.setLayout(new GridLayout(4,4,5,5));
+		
+		n.add(new JButton("7"));
+		n.add(new JButton("8"));
+		n.add(new JButton("9"));
+		n.add(new JButton("/"));
+		
+		n.add(new JButton("4"));
+		n.add(new JButton("5"));
+		n.add(new JButton("6"));
+		n.add(new JButton("*"));
+		
+		n.add(new JButton("1"));
+		n.add(new JButton("2"));
+		n.add(new JButton("3"));
+		n.add(new JButton("+"));
+		
+		n.add(new JButton("."));
+		n.add(new JButton("0"));
+		n.add(new JButton("-"));
+		n.add(new JButton("="));
+		
+		JTextField display = new JTextField();
+		display.setFont(new Font("Serif",Font.PLAIN, 13));
+		
 		
 		Container c = getContentPane();
-		c.add(BorderLayout.NORTH, new JTextField());
+		c.add(BorderLayout.NORTH, display);
 		c.add(BorderLayout.CENTER, n);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
