ba284e86974a48f6f8a67a8f93af110bfd374432
Jonathan Almeida <finigans@outlook.com.br>
2024-10-09 14:21:46 -0300

    introduzindo o Layout

--- src/Intro/Calc.java
+++ src/Intro/Calc.java
@@ -1,11 +1,25 @@
 package Intro;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Calc extends JFrame{
 	
 	public Calc() {
 		super("calculadora");
+		
+		Container n = new JPanel();
+		n.setLayout(new GridLayout(4,4));
+		
+		Container c = getContentPane();
+		c.add(BorderLayout.NORTH, new JTextField());
+		c.add(BorderLayout.CENTER, n);
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(300,300);
